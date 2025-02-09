6d0fa934113c8501d70294d261dce5c3ad2c44ce
samjmccormick <samjmccormick@gmail.com>
2025-02-08 17:11:07 -0800

    formatted email/username and sign-up form

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -4,6 +4,10 @@ const db = require("../db/queries.js");
 
 postsRouter.get("/", async (req, res) => {
   const messages = await db.getAllMessages();
+  messages.map((message) => {
+    message.timestamp = new Date(message.timestamp).toLocaleString();
+    message.username = message.username.split("@")[0];
+  });
   res.render("posts/posts", { messages: messages });
 });
 
@@ -12,6 +16,9 @@ postsRouter.get("/new", (req, res) => res.render("posts/newPost"));
 postsRouter.post("/new", async (req, res) => {
   await db.postNewPost(req.body.text, res.locals.currentUser);
   const messages = await db.getAllMessages();
+  messages.map((message) => {
+    message.timestamp = new Date(message.timestamp).toLocaleString();
+  });
   res.render("posts/posts", { messages: messages });
 });
 
